039850f0a47058dc40a7624ba04000b568768640
veromejia <veromejia.q@gmail.com>
2019-07-27 00:42:44 -0700

    testing printf

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "holberton.h"
+/**
+ * _printf - functions that print an produces output according to a format.
+ * @format: is a character string.
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...);
+{
+}
