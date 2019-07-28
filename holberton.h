4e2b2b4c7765d36c8cb3b6fefea9b3ee785cedb3
veromejia <veromejia.q@gmail.com>
2019-07-27 21:44:34 -0700

    task 0

--- holberton.h
+++ holberton.h
@@ -1,8 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
-#include <starg.h>
+#include <stdarg.h>
 #include <unistd.h>
+
+/**
+ * struct v_types - Type of formats
+ *
+ * @valid: format acepted
+ * @f: The function associated
+ */
+
+typedef struct v_types
+{
+	char *valid;
+	void (*f)();
+} types;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
