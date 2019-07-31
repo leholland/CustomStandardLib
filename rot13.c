c5b9c40202b79ee18e8eef3f3b49f4300fbcea60
Leah Holland <799@holbertonschool.com>
2019-07-31 01:20:22 +0000

    rot13

--- rot13.c
+++ rot13.c
@@ -1,24 +1,36 @@
 #include "holberton.h"
-
 /**
-* leet - Encodes string into 1337
-*@s: String to encode
-* Return: string
-*/
+ * rot13 - encode srting into rot13
+ * @ptr: string to encode
+ * Return: ptr
+ */
+int rot13(va_list s)
+{
+        char *str = va_arg(s, char *);
+        int x, y;
+        char alpha[56] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        char rot13[56] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-char *leet(char *s)
-{
-int i, k;
-int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+        for (x = 0; str[x] != '\0'; x++)
+        {
+                
+                        if (!((str[x] >= 'A' && str[x] <= 'Z') ||
+                (str[x] >= 'a' && str[x] <= 'z')))
+                                
+             _putchar(str[x]);
+    
+                for (y = 0; alpha[y] != '\0'; y++)
+                {
+                        if (str[x] == alpha[y])
+                        {     
+                               _putchar(rot13[y]);
+                               /* str[x] = rot13[y];*/
+                            
+                        }
+                    
+                   }
+    
+            }
+       return (y);
+}
 
-for (k = 0; s[k] != '\0'; k++)
-{
-for (i = 0; a[i] != '\0'; i++)
-{
-if (s[k] == a[i])
-s[k] = b[i];
-}
-}
-return (s);
-}
